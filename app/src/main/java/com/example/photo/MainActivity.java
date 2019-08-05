4d83dcd3e609974fbfe3edf21df776f52c33490e
Past1507 <p.stn@mail.ru>
2019-08-05 23:21:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/photo/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.photo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import java.util.Random;
+
+
+public class MainActivity extends AppCompatActivity {
+    TextView txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initActivity();
+    }
+
+    private void initActivity() {
+        Button btnForward = findViewById(R.id.btnForward);
+        txt = (TextView)findViewById(R.id.txt);
+
+        btnForward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,
+                        MainActivity.class);
+                startActivity(intent);
+                int randNum = new Random().nextInt(101);
+                txt.setText("https://myfile.org/"+randNum);
+            }
+        });
+
+        Button btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
